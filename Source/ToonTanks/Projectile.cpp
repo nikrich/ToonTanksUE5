f552b0cfa609bb0c988858b791ea6a27ebf3c500
nikrich <jannik811@gmail.com>
2022-10-28 08:49:55 +0200

    Added projectile

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -3,10 +3,27 @@
 
 #include "Projectile.h"
 
-Projectile::Projectile()
+// Sets default values
+AProjectile::AProjectile()
 {
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(RootComponent);
 }
 
-Projectile::~Projectile()
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
 {
+	Super::BeginPlay();
+	
 }
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -3,13 +3,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
 
-/**
- * 
- */
-class TOONTANKS_API Projectile
+UCLASS()
+class TOONTANKS_API AProjectile : public AActor
 {
-public:
-	Projectile();
-	~Projectile();
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		UStaticMeshComponent* BaseMesh;
+
 };
